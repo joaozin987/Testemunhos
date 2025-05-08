b56553839d57255f5f4166f838e551638aec1d59
João Paulo <joaosantoskp987@gmail.com>
2025-05-08 14:29:06 -0300

    upd

--- depoimentos/api/testemunhos.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-      res.status(200).json({ mensagem: 'API funcionando no Vercel!' });
-    } else {
-      res.status(405).end('Método não permitido');
-    }
-  }
-  
