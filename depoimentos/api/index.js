ef8a8cf6afcf688614f6d74d044dbc8e8150f5f5
joaozin987 <joaosantoskp987@gmail.com>
2025-08-18 14:55:50 -0300

    Finaliza ajustes do backend

--- depoimentos/api/index.js
+++ depoimentos/api/index.js
@@ -37,6 +37,7 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(express.json());
