56b26354b234e59cc7cd3f3c3885e3fbaa28a6c1
joaozin987 <joaosantoskp987@gmail.com>
2025-08-18 16:57:56 -0300

    ip

--- depoimentos/meu-frontend/src/api.js
+++ depoimentos/meu-frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // A URL base do seu servidor backend
-  baseURL: 'http://localhost:3000'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000', 
+  withCredentials: true, 
 });
 
 export default api;
