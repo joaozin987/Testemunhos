ace01918bc6635da988fc1af60e3e377ec86ef4d
joaozin987 <joaosantoskp987@gmail.com>
2025-11-25 19:53:21 -0300

    update

--- backend-laravel/config/cors.php
+++ backend-laravel/config/cors.php
@@ -2,33 +2,23 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-   'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
 
-'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://conectados-pela-fe.onrender.com',
+    ],
 
-'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
 
-'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
 
-'allowed_headers' => ['*'],
+    'exposed_headers' => [],
 
-'exposed_headers' => ['Authorization'],
+    'max_age' => 0,
 
-'max_age' => 0,
-
-'supports_credentials' => true,
-
+    'supports_credentials' => true,
 ];
