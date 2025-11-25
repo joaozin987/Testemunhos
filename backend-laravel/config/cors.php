53c26a6213c733200085d2827cb7de3456327384
joaozin987 <joaosantoskp987@gmail.com>
2025-11-24 21:11:57 -0300

    up

--- backend-laravel/config/cors.php
+++ backend-laravel/config/cors.php
@@ -22,7 +22,7 @@ return [
 'allowed_origins' => [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://seu-dominio.com', // já deixa preparado pro deploy
+    'https://conectados-pela-fe.onrender.com', // já deixa preparado pro deploy
 ],
 
 'allowed_origins_patterns' => [],
