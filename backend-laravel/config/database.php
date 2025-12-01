976b4c57f16d1d74b2f230095ca873be61bbcba5
Joaosantoszz <142459125+joaozin987@users.noreply.github.com>
2025-12-01 09:17:32 -0300

    Update MySQL configuration in database.php

--- backend-laravel/config/database.php
+++ backend-laravel/config/database.php
@@ -43,18 +43,24 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-      'mysql' => [
+    'mysql' => [
     'driver' => 'mysql',
-    'host' => env('DB_HOST'),
-    'port' => env('DB_PORT'),
-    'database' => env('DB_DATABASE'),
-    'username' => env('DB_USERNAME'),
-    'password' => env('DB_PASSWORD'),
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
-    'sslmode' => env('DB_SSL_MODE', 'require'),
+    'prefix' => '',
+    'strict' => true,
+    'engine' => null,
+
     'options' => array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
+        PDO::MYSQL_ATTR_SSL_CA => storage_path('app/ca.pem'),
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ]),
 ],
 
