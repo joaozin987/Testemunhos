f072ae5cb550b6a49f132b79ec452ebbbadc9223
Joaosantoszz <142459125+joaozin987@users.noreply.github.com>
2025-12-01 08:51:12 -0300

    Update database.php

--- backend-laravel/config/database.php
+++ backend-laravel/config/database.php
@@ -43,25 +43,21 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'engine' => null,
-            'strict' => true,
-              'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
-            ]) : [],
-        ],
+      'mysql' => [
+    'driver' => 'mysql',
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'sslmode' => env('DB_SSL_MODE', 'require'),
+    'options' => array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
+    ]),
+],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
