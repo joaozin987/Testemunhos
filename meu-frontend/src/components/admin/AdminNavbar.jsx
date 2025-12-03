85ae6a20e2824fcf708ab91e9d4651285f160403
joaozin987 <joaosantoskp987@gmail.com>
2025-12-03 20:43:25 -0300

    update admin

--- meu-frontend/src/components/admin/AdminNavbar.jsx
+++ meu-frontend/src/components/admin/AdminNavbar.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { useAuth } from '../../context/AuthContext.jsx';
 import { useNavigate } from 'react-router-dom';
 
-
 function AdminNavbar() {
-  const { usuario, logout } = useAuth();
-  <span>Olá, {user?.nome}</span>
+  const { user, logout } = useAuth(); 
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -13,14 +11,12 @@ function AdminNavbar() {
     navigate('/');
   };
 
-
-
   return (
     <header className="bg-white shadow-sm border-b">
       <div className="flex justify-between items-center px-6 py-4">
         <h1 className="text-xl font-semibold">Painel Administrativo</h1>
         <div className="flex items-center space-x-4">
-          <span>Olá,Chefe como o senhor vai? {usuario?.nome}</span>
+          <span>Olá, {user?.nome}</span>
           <button
             onClick={handleLogout}
             className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
