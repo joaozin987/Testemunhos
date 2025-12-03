3f17995cd5317a1b671f05d300d146ed98f60b47
joaozin987 <joaosantoskp987@gmail.com>
2025-12-03 18:40:30 -0300

    upd button

--- meu-frontend/src/pages/LoginPage.jsx
+++ meu-frontend/src/pages/LoginPage.jsx
@@ -85,7 +85,7 @@ function LoginPage() {
 
         <button
           type="submit"
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 hover:bg-blue-700 disabled:bg-blue-300"
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-5 hover:bg-blue-700 disabled:bg-blue-300"
           disabled={isLoading}
         >
           {isLoading ? "Entrando..." : "Continuar"}
